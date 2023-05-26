f000c1a9977e2358b4e705e530c8ad4e16a3208d
OussamaELhansali <102356792+OussamaELhansali@users.noreply.github.com>
2023-05-26 16:05:42 +0200

    "Bilan de Périodicité des changements de contenus par Mode + Sessions : 12-13-14 "

--- periodicity.py
+++ periodicity.py
@@ -177,49 +177,65 @@ def periodicity_average(files):
 def affichage_periodicité_mode(tab_event, mode):
     tableau = tableau_données(tab_event)[0]
     tableau = tableau[tableau["mod"] == mode]
-    tableau.at[0, "chgtMatiere"] = "Y"
-    tableau.at[0, "chgtNotion"] = "Y"
-    Y = list(tableau["chgtMatiere"][tableau["chgtMatiere"] == "Y"].index)
-    Y.sort()
+    tableau.at[tableau.index[0], "chgtMatiere"] = "Y"
+    tableau.at[tableau.index[0], "chgtNotion"] = "Y"
+    diff_indices = tableau.index.to_series().diff().fillna(1)
+    # Identifiez les indices consécutifs
+    group_indices = (diff_indices != 1).cumsum()
+    # Groupez les indices consécutifs et créez des petits tableaux
+    grouped_data = tableau.groupby(group_indices)
+    # Parcourez chaque groupe et récupérez les petits tableaux
+    small_tables = [grouped_data.get_group(group) for group in grouped_data.groups]
+
+    nb_questions = np.array([i.shape[0] for i in small_tables])
     l = []
     l_mod = []
-    if len(Y) < 2:
-        pass
-    else:
-        for i in range(len(Y) - 1):
-            l.append(Y[i + 1] - Y[i])
-            l_mod.append(str(Y[i + 1] - Y[i]) + str(tableau["mod"][Y[i]])[0])
-        l = np.array(l)
-        # print("le nombre de questions avant chaque changement de matière :", l_mod)
-        # print(
-        #     f"le nombre de questions moyen avant changement de matière est : {round(np.mean(l))}"
-        # )
-        # print(
-        #     f"l'ecart type du nombre de questions avant changement de notion est : {round(np.std(l))}"
-        # )
-        # print(
-        #     "################################################################################"
-        # )
+    i = 1
+    for table in small_tables:
+        table.at[table.index[0], "chgtMatiere"] = "Y"
+        table.at[table.index[0], "chgtNotion"] = "Y"
+        Y = list(table["chgtMatiere"][table["chgtMatiere"] == "Y"].index)
+        Y.sort()
 
-    W = list(tableau["chgtNotion"][tableau["chgtNotion"] == "Y"].index)
-    W.sort()
+        if len(Y) < 1:
+            pass
+        else:
+            for i in range(len(Y) - 1):
+                l.append(Y[i + 1] - Y[i])
+                l_mod.append(str(Y[i + 1] - Y[i]) + str(tableau["mod"][Y[i]])[0])
+    l = np.array(l)
+
+    # print("le nombre de questions avant chaque changement de matière :", l_mod)
+    # print(
+    #     f"le nombre de questions moyen avant changement de matière est : {round(np.mean(l))}"
+    # )
+    # print(
+    #     f"l'ecart type du nombre de questions avant changement de notion est : {round(np.std(l))}"
+    # )
+    # print(
+    #     "################################################################################"
+    # )
     n = []
-    if len(W) < 2:
-        pass
-    else:
-        for i in range(len(W) - 1):
-            n.append(W[i + 1] - W[i])
-        n = np.array(n)
-        # print("le nombre de questions avant chaque changement de notion :", n)
-        # print(
-        #     f"le nombre de questions moyen avant changement de notion est : {round(np.mean(n))}"
-        # )
-        # print(
-        #     f"l'ecart type du nombre de questions avant changement de notion est : {round(np.std(n))}"
-        # )
-        # print(
-        #     "################################################################################"
-        # )
+    for table in small_tables:
+        W = list(table["chgtNotion"][table["chgtNotion"] == "Y"].index)
+        W.sort()
+
+        if len(W) < 2:
+            pass
+        else:
+            for i in range(len(W) - 1):
+                n.append(W[i + 1] - W[i])
+    n = np.array(n)
+    # print("le nombre de questions avant chaque changement de notion :", n)
+    # print(
+    #     f"le nombre de questions moyen avant changement de notion est : {round(np.mean(n))}"
+    # )
+    # print(
+    #     f"l'ecart type du nombre de questions avant changement de notion est : {round(np.std(n))}"
+    # )
+    # print(
+    #     "################################################################################"
+    # )
 
     indices = np.where(tableau["mod"].ne(tableau["mod"].shift()))[0]
     w = []
@@ -246,70 +262,56 @@ def affichage_periodicité_mode(tab_event, mode):
     #     "################################################################################"
     # )
 
-    if len(indices) >= 3 and len(W) >= 2 and len(Y) >= 2:
+    if len(W) >= 2 and len(Y) >= 2:
         dico = {
-            "mean": [round(np.mean(l)), round(np.mean(n)), round(np.mean(w))],
-            "std": [round(np.std(l)), round(np.std(n)), round(np.std(w))],
+            "mean": [round(np.mean(l)), round(np.mean(n))],
+            "std": [round(np.std(l)), round(np.std(n))],
         }
         indexes = [
+            "nombre de questions",
             "changement de matière",
             "changement de notion",
-            "changement de mode",
         ]
+        dico["mean"] = [round(np.mean(nb_questions))] + dico["mean"]
+        dico["std"] = [round(np.std(nb_questions))] + dico["std"]
         mean_std = pd.DataFrame(dico, index=indexes)
         return mean_std
-    elif len(indices) < 3 and len(W) >= 2 and len(Y) >= 2:
+    elif len(W) >= 2 and len(Y) < 2:
         dico = {
-            "mean": [round(np.mean(l)), round(np.mean(n)), tableau.shape[0]],
-            "std": [round(np.std(l)), round(np.std(n)), 0],
+            "mean": [l[-1], round(np.mean(n))],
+            "std": [0, round(np.std(n))],
         }
         indexes = [
+            "nombre de questions",
             "changement de matière",
             "changement de notion",
-            "changement de mode",
         ]
-        mean_std = pd.DataFrame(dico, index=indexes)
-        return mean_std
-    elif len(W) >= 2 and len(Y) < 2 and len(indices) < 3:
-        dico = {
-            "mean": [tableau.shape[0], round(np.mean(n)), tableau.shape[0]],
-            "std": [0, round(np.std(n)), 0],
-        }
-        indexes = [
-            "changement de matière",
-            "changement de notion",
-            "changement de mode",
-        ]
-        mean_std = pd.DataFrame(dico, index=indexes)
-        return mean_std
-    elif len(indices) >= 3 and len(Y) < 2 and len(W) < 2:
-        dico = {
-            "mean": [tableau.shape[0], tableau.shape[0], round(np.mean(w))],
-            "std": [0, 0, round(np.std(w))],
-        }
-        indexes = [
-            "changement de matière",
-            "changement de notion",
-            "changement de mode",
-        ]
+        dico["mean"] = [round(np.mean(nb_questions))] + dico["mean"]
+        dico["std"] = [round(np.mean(nb_questions))] + dico["std"]
         mean_std = pd.DataFrame(dico, index=indexes)
         return mean_std
     else:
         dico = {
-            "mean": [round(np.mean(l)), round(np.mean(n)), round(np.mean(w))],
-            "std": [round(np.std(l)), round(np.std(n)), round(np.std(w))],
+            "mean": [l[-1], n[-1]],
+            "std": [0, 0],
         }
         indexes = [
+            "nombre de questions",
             "changement de matière",
             "changement de notion",
-            "changement de mode",
         ]
+        dico["mean"] = [round(np.mean(nb_questions))] + dico["mean"]
+        dico["std"] = [round(np.std(nb_questions))] + dico["std"]
         mean_std = pd.DataFrame(dico, index=indexes)
+
         return mean_std
 
 
+file_names = ["S12_nonParam.json", "S13_nonParam.json", "S14_nonParam.json"]
+
+
 def periodicity_average_mode(files, mode):
-    dico = {"mean": [[] for i in range(2)], "std": [[] for i in range(2)]}
+    dico = {"mean": [[] for i in range(3)], "std": [[] for i in range(3)]}
     j = 0
     for i in files:
         # display(
@@ -323,17 +325,17 @@ def periodicity_average_mode(files, mode):
         x
         dico["mean"][0].append(x["mean"][0])
         dico["mean"][1].append(x["mean"][1])
-        # dico["mean"][2].append(x["mean"][2])
+        dico["mean"][2].append(x["mean"][2])
         dico["std"][0].append(x["std"][0])
         dico["std"][1].append(x["std"][1])
-        # dico["std"][2].append(x["std"][2])
+        dico["std"][2].append(x["std"][2])
         j += 1
     dico["mean"] = [round(np.mean(np.array(i))) for i in dico["mean"]]
     dico["std"] = [round(np.mean(np.array(i))) for i in dico["std"]]
     indexes = [
+        "nombre de questions moyen",
         "changement de matière moyen",
         "changement de notion moyen",
-        # "changement de mode moyen",
     ]
     display(
         HTML(
@@ -342,3 +344,6 @@ def periodicity_average_mode(files, mode):
     )
     dico = pd.DataFrame(dico, index=indexes)
     return dico
+
+
+print(periodicity_average_mode(file_names, "revis"))
